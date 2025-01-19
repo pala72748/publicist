dc03828cea06348c7bce0bfe757e2f3f47c61390
pala72748 <pala72748@gmail.com>
2025-01-19 20:33:52 +0530

    new

---
+++ index.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Publicist - Digital Marketing Agency</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://code.iconify.design/iconify-icon/2.3.0/iconify-icon.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+        rel="stylesheet">
+</head>
+
+<body class="bg-background-950 h-screen font-body">
+    <header id="header" class="bg-background-50 fixed top-0 left-0 w-full h-20 z-50 transition-transform transform">
+        <div class="container mx-auto">
+            <div class="flex flex-row justify-between items-center h-full">
+                <div class="">
+                    <img src="https://thepublicist.co/wp-content/uploads/2024/10/Full-logo-1.png" width="290px" alt="">
+                </div>
+                <div class="w-8/12">
+                    <ul class="flex flex-row  justify-center text-white gap-8 text-xl">
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">About</a>
+                        </li>
+                        <li>
+                            <a href="">Services</a>
+                        </li>
+                        <li>
+                            <a href="">Blog</a>
+                        </li>
+                        <li>
+                            <a href="">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="">
+                    <button class="py-2 px-4 bg-background-500 rounded-lg text-xl uppercase flex items-center">Get
+                        Started
+                        <iconify-icon class="rotate-45" icon="prime:arrow-up" width="30" height="30"></iconify-icon>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="w-50">
+        <video muted loop autoplay src="assets/video/website_bg.webm"></video>
+    </section>
+
+    <section class="h-50 bg-gray-100 py-40 flex flex-col gap-8">
+        <div class="flex flex-col items-center gap-2 w-full">
+            <h4 class="text-xl">The Publicist</h4>
+            <h1 class=" text-4xl">Our Clients</h1>
+        </div>
+        <div class=" container mx-auto overflow-hidden whitespace-nowrap mt-8">
+            <div class="flex animate-marquee space-x-8 items-center gap-8 justify-center">
+                <img class="w-full " src="assets/img/client/1.png" alt="">
+                <img class="w-full " src="assets/img/client/2.png" alt="">
+                <img class="w-full " src="assets/img/client/3.png" alt="">
+                <img class="w-full " src="assets/img/client/4.png" alt="">
+                <img class="w-full " src="assets/img/client/5.png" alt="">
+                <img class="w-full " src="assets/img/client/1.png" alt="">
+                <img class="w-full " src="assets/img/client/2.png" alt="">
+                <img class="w-full " src="assets/img/client/3.png" alt="">
+                <img class="w-full " src="assets/img/client/4.png" alt="">
+                <img class="w-full " src="assets/img/client/5.png" alt="">
+                <img class="w-full " src="assets/img/client/1.png" alt="">
+                <img class="w-full " src="assets/img/client/2.png" alt="">
+                <img class="w-full " src="assets/img/client/3.png" alt="">
+                <img class="w-full " src="assets/img/client/4.png" alt="">
+                <img class="w-full " src="assets/img/client/5.png" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-background-50 py-40">
+        <div class="container mx-auto h-full">
+            <div class="flex flex-col justify-center items-center h-full">
+                <div class="flex flex-col text-white items-center gap-2 w-full">
+                    <h4 class="text-xl">The Publicist</h4>
+                    <h1 class=" text-4xl">Our Services</h1>
+                </div>
+            </div>
+            <div class="flex grid grid-cols-2 flex-row  justify-between gap-12">
+                <div class="w-full pt-20">
+                    <img class="rounded-xl" src="https://crowdytheme.com/html/arolax/assets/imgs/works/img-s-31.webp"
+                        alt="">
+                    <div class="text-white pt-5">
+                        <h1 class="text-3xl">Smart T-shirt Artwork</h1>
+                        <div class="pt-4 flex flex-row gap-4">
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Branding</span>
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Logo</span>
+                            <span class="text-white border border-white px-4 py-2 uppsercase rounded-full">Business</span>
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Designing</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full pt-20">
+                    <img class="rounded-xl" src="https://crowdytheme.com/html/arolax/assets/imgs/works/img-s-32.webp"
+                        alt="">
+                    <div class="text-white pt-5">
+                        <h1 class="text-3xl">Crafted Payment
+                        Get-way</h1>
+                        <div class="pt-4 flex flex-row gap-4">
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Branding</span>
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Logo</span>
+                            <span class="text-white border border-white px-4 py-2 uppsercase rounded-full">Business</span>
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Designing</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full pt-20">
+                    <img class="rounded-xl" src="https://crowdytheme.com/html/arolax/assets/imgs/works/img-s-33.webp"
+                        alt="">
+                    <div class="text-white pt-5">
+                        <h1 class="text-3xl">Wild Code Zem
+                        Boxes</h1>
+                        <div class="pt-4 flex flex-row gap-4">
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Branding</span>
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Logo</span>
+                            <span class="text-white border border-white px-4 py-2 uppsercase rounded-full">Business</span>
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Designing</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full pt-20">
+                    <img class="rounded-xl" src="https://crowdytheme.com/html/arolax/assets/imgs/works/img-s-34.webp"
+                        alt="">
+                    <div class="text-white pt-5">
+                        <h1 class="text-3xl">Floral Business
+                        Card</h1>
+                        <div class="pt-4 flex flex-row gap-4">
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Branding</span>
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Logo</span>
+                            <span class="text-white border border-white px-4 py-2 uppsercase rounded-full">Business</span>
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Designing</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full pt-20">
+                    <img class="rounded-xl" src="https://crowdytheme.com/html/arolax/assets/imgs/works/img-s-35.webp"
+                        alt="">
+                    <div class="text-white pt-5">
+                        <h1 class="text-3xl">Colyfate Font
+                        Design</h1>
+                        <div class="pt-4 flex flex-row gap-4">
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Branding</span>
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Logo</span>
+                            <span class="text-white border border-white px-4 py-2 uppsercase rounded-full">Business</span>
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Designing</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full pt-20">
+                    <img class="rounded-xl" src="https://crowdytheme.com/html/arolax/assets/imgs/works/img-s-36.webp"
+                        alt="">
+                    <div class="text-white pt-5">
+                        <h1 class="text-3xl">Watch Mockup
+                        Design</h1>
+                        <div class="pt-4 flex flex-row gap-4">
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Branding</span>
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Logo</span>
+                            <span class="text-white border border-white px-4 py-2 uppsercase rounded-full">Business</span>
+                            <span class="text-white border border-white px-4 py-2 uppercase rounded-full">Designing</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+
+
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    animation: {
+                        marquee: 'marquee 15s linear infinite',
+                    },
+                    keyframes: {
+                        marquee: {
+                            '0%': { transform: 'translateX(0)' },
+                            '100%': { transform: 'translateX(-50%)' },
+                        },
+                    },
+                    colors: {
+                        'text': {
+                            50: '#031709',
+                            100: '#052e13',
+                            200: '#0b5b26',
+                            300: '#108938',
+                            400: '#15b74b',
+                            500: '#1be45e',
+                            600: '#48ea7e',
+                            700: '#76ef9e',
+                            800: '#a4f4bf',
+                            900: '#d1fadf',
+                            950: '#e8fcef',
+                        },
+                        'background': {
+                            50: '#021706',
+                            100: '#042f0d',
+                            200: '#095d1a',
+                            300: '#0d8c26',
+                            400: '#11bb33',
+                            500: '#16e940',
+                            600: '#44ee66',
+                            700: '#73f28c',
+                            800: '#a2f6b3',
+                            900: '#d0fbd9',
+                            950: '#ffffff',
+                        },
+                        'primary': {
+                            50: '#031708',
+                            100: '#062d10',
+                            200: '#0c5a21',
+                            300: '#128731',
+                            400: '#17b541',
+                            500: '#1de252',
+                            600: '#4ae874',
+                            700: '#78ed97',
+                            800: '#a5f3ba',
+                            900: '#d2f9dc',
+                            950: '#e8fcee',
+                        },
+                        'secondary': {
+                            50: '#031217',
+                            100: '#06242d',
+                            200: '#0c485a',
+                            300: '#126c87',
+                            400: '#1790b5',
+                            500: '#1db4e2',
+                            600: '#4ac3e8',
+                            700: '#78d2ed',
+                            800: '#a5e1f3',
+                            900: '#d2f0f9',
+                            950: '#e8f7fc',
+                        },
+                        'accent': {
+                            50: '#030e17',
+                            100: '#061d2d',
+                            200: '#0c3a5a',
+                            300: '#125687',
+                            400: '#1773b5',
+                            500: '#1d90e2',
+                            600: '#4aa6e8',
+                            700: '#78bced',
+                            800: '#a5d3f3',
+                            900: '#d2e9f9',
+                            950: '#e8f4fc',
+                        },
+                    },
+                    fontSize: {
+                        sm: '0.750rem',
+                        base: '1rem',
+                        xl: '1.333rem',
+                        '2xl': '1.777rem',
+                        '3xl': '2.369rem',
+                        '4xl': '3.158rem',
+                        '5xl': '4.210rem',
+                    },
+                    fontFamily: {
+                        heading: 'DM Sans',
+                        body: 'DM Sans',
+                    },
+                    fontWeight: {
+                        normal: '400',
+                        bold: '700',
+                    },
+                }
+            }
+        }
+
+        document.addEventListener("scroll", () => {
+            const header = document.getElementById("header");
+            if (window.scrollY > 50) {
+                header.classList.add("translate-y-0", "opacity-100");
+                header.classList.remove("-translate-y-10", "opacity-0");
+            } else {
+                header.classList.add("-translate-y-10", "opacity-0");
+                header.classList.remove("translate-y-0", "opacity-100");
+            }
+        });
+
+    </script>
+</body>
+
+</html>
